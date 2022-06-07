4c8ea07ab7f505b5e94eef9fabe3a41b5771e714
okamotokeita <okamoto.keita1937@gmail.com>
2022-06-08 01:33:40 +0900

    Use Lists作成

--- lib/forth_page.dart
+++ lib/forth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wolf/next_page.dart';
 
 class forthPage extends StatelessWidget {
   // NextPageを作るときにnameを入れるようにする
@@ -13,16 +14,41 @@ class forthPage extends StatelessWidget {
         title: Text('4つ目の画面'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            RaisedButton(
-              child: const Text('戻る'),
-              onPressed: () {
-                Navigator.pop(context, '4つ目の画面から戻す値');
+        // リスト①
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('設定'),
+              trailing: Icon(Icons.access_alarm_outlined),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage('yosi')),
+                );
               },
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
             ),
           ],
         ),
+        // child: Column(
+        //   children: [
+        //
+        //     RaisedButton(
+        //       child: const Text('戻る'),
+        //       onPressed: () {
+        //         Navigator.pop(context, '4つ目の画面から戻す値');
+        //       },
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
