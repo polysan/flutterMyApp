bcf55baa502d100eb1cdc131b22f495772a869d3
okamotokeita <okamoto.keita1937@gmail.com>
2022-06-11 14:18:24 +0900

    タイトル変更＆fontawesomeインストール

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wolf/next_page.dart';
 
 void main() {
@@ -54,7 +55,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Row (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(FontAwesomeIcons.circle, color: Colors.green, size: 30),
+            Icon(Icons.clear, color: Colors.red, size: 30),
+            Text('ゲーム')
+          ],
+        ),
       ),
       body: Center(
         child: Column(
@@ -101,3 +109,14 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+//todo アプリのタイトルを変更
+//todo フィールドのUIを作成
+//todo フィールドのUI作成をメソッドを用いて簡潔に
+//todo ターンの表示とクリアボタンの作成
+//todo マス目をタップ可能にし、タップ時にターン切り替え
+//todo マス目タップでマルバツを表示
+//todo ゲームの勝敗のパターンを書き出す
+//todo ゲームの勝敗を判定可能に
+//todo リセットボタンタップでリスタート可能に
+//todo 勝敗をわかりやすく表示
